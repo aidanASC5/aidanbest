c6874e60d1329b99b7af71cbb6eeebae6c2e4f86
aidanASC5 <aidan@smithbest.com>
2018-07-13 09:51:00 -0300

    adding files

---
+++ aidan.js
@@ -0,0 +1,6 @@
+const ps4 = 299.99;
+const godOfWar = 59.99;
+const fortnite = 59.99;
+const detroitBecomeHuman = 59.99;
+const total = ps4 + godOfWar + fortnite + detroitBecomeHuman;
+console.log(total);
